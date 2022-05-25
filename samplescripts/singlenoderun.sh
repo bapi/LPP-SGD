ef4debe7cde04e2f74301903655ca21744fcaace
Bapi <bhaskerchatterjee@gmail.com>
2022-05-25 23:21:59 +0530

    changes

--- samplescripts/singlenoderun.sh
+++ samplescripts/singlenoderun.sh
@@ -17,7 +17,7 @@ cd rn20cifar10_single_node
 $MPIRUN -n $TOTALGPUS -H localhost:$TOTALGPUS -bind-to none -map-by slot \
 $PYTHON $PROGRAM --data-dir $DATADIR  \
 --dataset cifar10 --num-classes 10 --momentum 0.9 --model res20 \
---cuda --train_processing_bs 256 --test_processing_bs 256 --lr 0.4 \
+--cuda --train_processing_bs 128 --test_processing_bs 128 --lr 0.1 \
 --baseline_lr 0.1 --weight-decay 0.0005 --seed 6 --pm  \
 --nesterov --workers 0 --num-threads 2 --test-freq 1 --partition \
 --num-processes 4 --pre_post_epochs 150 --scheduler-type cosine \
@@ -30,7 +30,7 @@ $PYTHON $PROGRAM --data-dir $DATADIR  \
 $MPIRUN -n $TOTALGPUS -H localhost:$TOTALGPUS -bind-to none -map-by slot \
 $PYTHON $PROGRAM --data-dir $DATADIR  \
 --dataset cifar10 --num-classes 10 --momentum 0.9 --model res20 \
---cuda --train_processing_bs 256 --test_processing_bs 256 --lr 0.5 \
+--cuda --train_processing_bs 128 --test_processing_bs 128 --lr 0.125 \
 --baseline_lr 0.1 --weight-decay 0.0005 --seed 6 --pm  \
 --nesterov --workers 0 --num-threads 2 --test-freq 1 --partition \
 --num-processes 4 --pre_post_epochs 150 --scheduler-type cosine \
@@ -49,18 +49,29 @@ $PYTHON $PROGRAM --data-dir $DATADIR  \
 --scheduler-type mstep --training-type PLSGD  --pre_post_epochs 150 \
 --bs_multiple 1 --test_bs_multiple 1 --epochs 300 --warm_up_epochs 5 \
 --dist-url tcp://localhost:23456 --lrmilestone 150 225  --storeresults --lars
-#
-############################MBSGD#####################################
-#
+
+
 $MPIRUN -n $TOTALGPUS -H localhost:$TOTALGPUS -bind-to none -map-by slot \
 $PYTHON $PROGRAM --data-dir $DATADIR  \
 --dataset cifar10 --num-classes 10 --momentum 0.9 --model res20 \
---cuda --train_processing_bs 1024 --test_processing_bs 1024 --lr 0.8 \
+--cuda --train_processing_bs 128 --test_processing_bs 128 --lr 0.1 \
 --baseline_lr 0.1 --weight-decay 0.0005 --seed 0 --pm --averaging_freq 8 \
 --nesterov --workers 4 --num-threads 4 --test-freq 1 --partition \
---scheduler-type mstep --training-type MBSGD \
+--scheduler-type mstep --training-type PLSGD  --pre_post_epochs 150 \
 --bs_multiple 1 --test_bs_multiple 1 --epochs 300 --warm_up_epochs 5 \
 --dist-url tcp://localhost:23456 --lrmilestone 150 225  --storeresults --lars
+#
+############################MBSGD#####################################
+#
+# $MPIRUN -n $TOTALGPUS -H localhost:$TOTALGPUS -bind-to none -map-by slot \
+# $PYTHON $PROGRAM --data-dir $DATADIR  \
+# --dataset cifar10 --num-classes 10 --momentum 0.9 --model res20 \
+# --cuda --train_processing_bs 1024 --test_processing_bs 1024 --lr 0.8 \
+# --baseline_lr 0.1 --weight-decay 0.0005 --seed 0 --pm --averaging_freq 8 \
+# --nesterov --workers 4 --num-threads 4 --test-freq 1 --partition \
+# --scheduler-type mstep --training-type MBSGD \
+# --bs_multiple 1 --test_bs_multiple 1 --epochs 300 --warm_up_epochs 5 \
+# --dist-url tcp://localhost:23456 --lrmilestone 150 225  --storeresults --lars
 #
 ############################PLSGD+LARS#####################################
 #
